0e0e7df69073a80014a21a4e08da093b1daa0b7f
PaulXrP <pranaypaul@icloud.com>
2025-04-09 11:47:17 +0530

    Initial commit...Spring Boot setup

---
+++ src/main/java/com/jpa/example/spring_hibernate_props/SpringHibernatePropsApplication.java
@@ -0,0 +1,13 @@
+package com.jpa.example.spring_hibernate_props;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHibernatePropsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHibernatePropsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jpa/example/spring_hibernate_props/SpringHibernatePropsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpa.example.spring_hibernate_props;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHibernatePropsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
